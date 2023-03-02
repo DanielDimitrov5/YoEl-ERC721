2d8aafbecb513e1dfae867706c24efe9ae087157
Daniel Dimitrov <danieldimitrov2304@gmail.com>
2023-03-02 17:33:43 +0200

    Add vrf chainlink

--- src/YoEl.sol
+++ src/YoEl.sol
@@ -3,13 +3,17 @@ pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 contract YoEl is ERC721URIStorage {
 
     uint256 public constant MAX_SUPPLY = 10000;
     uint256 private _currentTokenId = 0;
 
-    constructor() ERC721("YoEl Coin", "ERC721") {
-        _mint(msg.sender, 1);
+    constructor() ERC721("YoEl Coin", "YoEl") {
+        _mint(msg.sender, _currentTokenId);
+        //s
     }
 
     function mint(address to) public payable {
