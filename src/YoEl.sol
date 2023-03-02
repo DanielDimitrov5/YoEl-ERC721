bed454cb9172ad065b7609e109fc6ada559935cc
Daniel Dimitrov <danieldimitrov2304@gmail.com>
2023-03-02 17:53:09 +0200

    chainlink VRFv2 initial implementation

--- src/YoEl.sol
+++ src/YoEl.sol
@@ -2,24 +2,55 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
-contract YoEl is ERC721URIStorage {
+contract YoEl is ERC721URIStorage, VRFConsumerBaseV2, Ownable {
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    // NFT Variables
+    uint256 private immutable i_mintFee;
+    uint256 private s_tokenCounter;
+    uint256 internal constant MAX_CHANCE_VALUE = 100;
+    string[] internal s_dogTokenUris;
+    bool private s_initialized;
 
     uint256 public constant MAX_SUPPLY = 10000;
     uint256 private _currentTokenId = 0;
 
-    constructor() ERC721("YoEl Coin", "YoEl") {
-        _mint(msg.sender, _currentTokenId);
-        //s
+    constructor(
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane, // keyHash
+        uint256 mintFee,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("YoEl", "YoEl") {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_mintFee = mintFee;
+        i_callbackGasLimit = callbackGasLimit;
+        s_tokenCounter = 0;
     }
 
     function mint(address to) public payable {
         require(msg.value >= 0.01 ether, "YoEl: not enough ETH");
         require(_currentTokenId < MAX_SUPPLY, "YoEl: max supply reached");
 
+            uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
         _mint(to, _currentTokenId);
         _currentTokenId++;
     }
@@ -27,4 +58,8 @@ contract YoEl is ERC721URIStorage {
     function setUrl(uint256 tokenId, string memory _tokenURI) public {
         _setTokenURI(tokenId, _tokenURI);
     }
+
+     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        
+     }
 }
