f405b8d175f43f2fd7836eadfd84fa8aae42dfa4
Daniel Dimitrov <danieldimitrov2304@gmail.com>
2023-03-03 12:36:26 +0200

    implement nft contract

--- src/YoEl.sol
+++ src/YoEl.sol
@@ -7,7 +7,18 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
+error RandomIpfsNft__AlreadyInitialized();
+error RandomIpfsNft__NeedMoreETHSent();
+error RandomIpfsNft__TransferFailed();
+
 contract YoEl is ERC721URIStorage, VRFConsumerBaseV2, Ownable {
+    // Types
+    enum Type {
+        Yo,
+        El
+    }
+
+    // Chainlink VRF Variables
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
     uint64 private immutable i_subscriptionId;
     bytes32 private immutable i_gasLane;
@@ -16,50 +27,103 @@ contract YoEl is ERC721URIStorage, VRFConsumerBaseV2, Ownable {
     uint32 private constant NUM_WORDS = 1;
 
     // NFT Variables
-    uint256 private immutable i_mintFee;
+    uint256 private i_mintFee;
     uint256 private s_tokenCounter;
     uint256 internal constant MAX_CHANCE_VALUE = 100;
-    string[] internal s_dogTokenUris;
+    string[] internal s_tokenUris;
     bool private s_initialized;
 
-    uint256 public constant MAX_SUPPLY = 10000;
-    uint256 private _currentTokenId = 0;
+    // VRF Helpers
+    mapping(uint256 => address) public s_requestIdToSender;
+
+    // Events
+    event NftRequested(uint256 indexed requestId, address requester);
+    event NftMinted(Type breed, address minter);
 
     constructor(
-        address vrfCoordinatorV2,
+        address vrfCoordinatorV2, 
         uint64 subscriptionId,
-        bytes32 gasLane, // keyHash
+        bytes32 gasLane,
         uint256 mintFee,
-        uint32 callbackGasLimit
+        uint32 callbackGasLimit,
+        string[2] memory tokenUris
     ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("YoEl", "YoEl") {
         i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
         i_gasLane = gasLane;
         i_subscriptionId = subscriptionId;
         i_mintFee = mintFee;
         i_callbackGasLimit = callbackGasLimit;
+        _initializeContract(tokenUris);
         s_tokenCounter = 0;
     }
 
-    function mint(address to) public payable {
-        require(msg.value >= 0.01 ether, "YoEl: not enough ETH");
-        require(_currentTokenId < MAX_SUPPLY, "YoEl: max supply reached");
-
-            uint256 requestId = i_vrfCoordinator.requestRandomWords(
+    function requestNft() public payable returns (uint256 requestId) {
+        if (msg.value < i_mintFee) {
+            revert RandomIpfsNft__NeedMoreETHSent();
+        }
+        requestId = i_vrfCoordinator.requestRandomWords(
             i_gasLane,
             i_subscriptionId,
             REQUEST_CONFIRMATIONS,
             i_callbackGasLimit,
             NUM_WORDS
         );
-        _mint(to, _currentTokenId);
-        _currentTokenId++;
+
+        s_requestIdToSender[requestId] = msg.sender;
+        emit NftRequested(requestId, msg.sender);
     }
 
-    function setUrl(uint256 tokenId, string memory _tokenURI) public {
-        _setTokenURI(tokenId, _tokenURI);
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+        address sender = s_requestIdToSender[requestId];
+        uint256 newItemId = s_tokenCounter;
+        s_tokenCounter = s_tokenCounter + 1;
+
+        uint256 rng = randomWords[0];
+        Type nftType = getTypeFromRNG(rng);
+        _safeMint(sender, newItemId);
+        _setTokenURI(newItemId, s_tokenUris[uint256(nftType)]);
+
+        emit NftMinted(nftType, sender);
     }
 
-     function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
-        
-     }
+    function _initializeContract(string[2] memory tokenUris) private {
+        if (s_initialized) {
+            revert RandomIpfsNft__AlreadyInitialized();
+        }
+        s_tokenUris = tokenUris;
+        s_initialized = true;
+    }
+
+    function getTypeFromRNG(uint256 rng) public pure returns (Type) {
+        return Type(rng % 2);
+    }
+
+    function withdraw() public onlyOwner {
+        uint256 amount = address(this).balance;
+        (bool success, ) = payable(msg.sender).call{value: amount}("");
+        if (!success) {
+            revert RandomIpfsNft__TransferFailed();
+        }
+    }
+
+    function changeFee(uint256 newFee) public onlyOwner {
+        require(newFee > 0, "Fee must be greater than 0");
+        i_mintFee = newFee;
+    }
+
+    function getMintFee() public view returns (uint256) {
+        return i_mintFee;
+    }
+
+    function getTokenUris(uint256 index) public view returns (string memory) {
+        return s_tokenUris[index];
+    }
+
+    function getInitialized() public view returns (bool) {
+        return s_initialized;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
 }
